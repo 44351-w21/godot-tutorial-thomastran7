4a7c0df7bfaaf809691e4fbd3f045592e3bfad69
Thomas Tran <thomastran707@gmail.com>
2021-01-20 13:44:10 -0600

    Work in Progress: Implemented player movement

---
+++ Player.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+export var speed = 400
+var screen_size 
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		#AnimitedSprite gets the child node that is called AnimatedSprite
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
