1e62cc03a70ff989464a0945315c8b757fdcc696
Thomas Tran <thomastran707@gmail.com>
2021-01-25 10:49:44 -0600

    get game playable

--- Main.gd
+++ Main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+export (PackedScene) var Mob
+var score
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,9 +11,42 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#temp so we can deploy
-	$Player.start(Vector2())
+	#$Player.start(Vector2())
+	randomize()
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.offset = randi()
+	var mob  = Mob.instance()
+	add_child(mob)
+	var direction = $MobPath/MobSpawnLocation.rotation + PI/2
+	mob.position = $MobPath/MobSpawnLocation.position
+	direction += rand_range(-PI/4, PI/4)
+	mob.rotation = direction
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
